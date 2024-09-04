c3b6432246237555bc222429f1ef87df0223d807
HVSharma12 <harshvardhan.sharma@suse.com>
2024-09-04 18:40:04 +0530

    Auto-committed by Ansible

--- containers/X11/entrypoint.sh
+++ containers/X11/entrypoint.sh
@@ -23,12 +23,11 @@ rm -f /tmp/.X${DISPLAY_NUM}-lock /tmp/.X11-unix/X${DISPLAY_NUM}
 # Create .xauth file before invoking startx
 [ ! -d "/home/user/xauthority" ] && mkdir -p "/home/user/xauthority"
 touch /home/user/xauthority/.xauth
-chown -R user:users /home/user/xauthority
 
 if [ $# -gt 0 ]; then
     log "Executing custom command: $@"
     exec "$@"
 else
     log "Starting X server on display $DISPLAY"
-    su - user -c "startx -- $DISPLAY"
+    exec startx -- "$DISPLAY"
 fi
