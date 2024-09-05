02fdedfbe0b5f4da3f5359baecd8e4fbb49fe612
HVSharma12 <harshvardhan.sharma@suse.com>
2024-09-06 00:40:15 +0530

    Auto-committed by Ansible

--- containers/X11/entrypoint.sh
+++ containers/X11/entrypoint.sh
@@ -7,7 +7,7 @@ log() {
 
 log "Starting X11 entrypoint script"
 
-# Trap signals (SIGTERM, SIGINT) for graceful shutdown
+# Trap signals for graceful shutdown
 trap 'log "Received SIGTERM, initiating shutdown"; cleanup' SIGTERM
 trap 'log "Received SIGINT, initiating shutdown"; cleanup' SIGINT
 
@@ -37,19 +37,10 @@ DISPLAY_NUM=$(echo $DISPLAY | sed 's/^://')
 log "Cleaning up existing X server lock files for display $DISPLAY"
 rm -f /tmp/.X${DISPLAY_NUM}-lock /tmp/.X11-unix/X${DISPLAY_NUM}
 
-# If a custom command is passed, run it
 if [ $# -gt 0 ]; then
     log "Executing custom command: $@"
     exec "$@"
 else
-    # Start X server
     log "Starting X server on display $DISPLAY"
-    startx -- "$DISPLAY" &
-    X_PID=$!
-
-    # Wait for X server process (Xorg) to finish
-    log "X server (startx) running with PID $X_PID"
-    wait $X_PID
-
-    log "X server has exited"
+    exec startx -- "$DISPLAY"
 fi
